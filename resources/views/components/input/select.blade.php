79fe188b365b62c390c92c99b96c14f8c6747c31
a.zemlyanov <a.zemlyanov@framework.team>
2022-03-15 16:06:45 +0400

    fix_select_components

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,6 +1,5 @@
 <select name="group_id" class="form-select mb-3 lead" style="max-width: max-content;">
-    <option value="{{ $student->group->id }}" selected> {{$student->group->name}} </option>
     @foreach ($groups as $name => $id)
-    <option value="{{ $id }}"> {{ $name }}</option>
+    <option value="{{ $id }}" @if($student->group->id === $id) selected @endif> {{ $name }}</option>
     @endforeach
 </select>
