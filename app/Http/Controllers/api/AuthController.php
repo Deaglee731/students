6aa9e7c17e53e826d5abc170307a779eafc664cf
a.zemlyanov <a.zemlyanov@framework.team>
2022-03-28 11:14:23 +0400

    create_api_auth

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,23 +4,45 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Composer\DependencyResolver\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class AuthController extends Controller
 {
     public function login (LoginRequest $request)
     {
-        //
+        $request->authenticate();
+
+        $token = $request->user()->createToken($request->email)->accessToken;
+
+        return response()->json([
+            'token' => $token,
+            'message' => 'Sucessfull',
+        ]);
     }
 
-    public function logoun (Request $request)
+    public function logout (Request $request)
     {
-        //
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Sucessfull',
+        ]);
     }
 
     public function resetPassword (Request $request)
     {
-        // 
+        $request->validate([
+            'email' => 'required',
+        ]);
+        
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return response()->json([
+            'status' => $status,
+        ]);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\GroupController;
+use App\Http\Controllers\api\StudentController;
+use App\Http\Controllers\api\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +22,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::POST('login', [AuthController::class, 'login'])->name('api.login');
+
+Route::POST('reset-password', [AuthController::class, 'resetPassword'])->name('api.reset.password');
+
 Route::middleware('auth:api')->group(function () {
-    Route::GET('login', [AuthController::class, 'login'])->name('api.login');
 
-    Route::POST('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::POST('logout', [AuthController::class, 'logout'])->name('api.logout');
 
-    Route::POST('reset-password', [AuthController::class, 'resetPassword'])->name('auth.reset.password');
+    Route::resource('groups', GroupController::class)->shallow()->middleware('auth:api');
+    Route::resource('subjects', SubjectController::class)->shallow()->middleware('auth:api');
+    Route::resource('students', StudentController::class)->shallow()->middleware('auth:api');
+
+    Route::GET('/students/{student}', [StudentController::class, 'show'])
+        ->name('students.show')
+        ->withTrashed()
+        ->middleware('auth:api');
 });
